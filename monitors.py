875ce7746df3981e7ea7789805e414517f39ffba
Pranja Roy Vishvakarma <55049003+AlwaysSmiling@users.noreply.github.com>
2022-04-10 10:01:11 +0530

    added headers in request

--- monitors.py
+++ monitors.py
@@ -21,7 +21,8 @@ class Monitor:
 
     def ini(self) -> str:
         """Initially called to retrieve latestchapter Chapter name at obj creation."""
-        response = requests.get(self.url)
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46'}
+        response = requests.get(self.url, headers=headers)
         return BeautifulSoup(response.text, 'lxml',
                              parse_only=self.strainer).get_text()
 
